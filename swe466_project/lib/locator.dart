2af8c60e37093a9a8d43ee4c4b065e4904abf8a7
HaneenAlmohaimeed <60116673+halmohaimeed@users.noreply.github.com>
2021-04-01 03:07:30 +0300

    adding stacked package

--- swe466_project/lib/locator.dart
+++ swe466_project/lib/locator.dart
@@ -1,7 +1,8 @@
 import 'package:get_it/get_it.dart';
+import 'package:swe466_project/viewmodels/homeViewModel.dart';
 
 GetIt locator = GetIt.instance;
 
 void setupLocator() {
-
+  locator.registerFactory(() => HomeViewModel());
 }
