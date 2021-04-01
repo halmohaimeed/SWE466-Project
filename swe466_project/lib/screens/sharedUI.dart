6c15fcd8d1e0dae539f443a8d863c15b00f89ba2
Lama <lamayf@hotmail.com>
2021-04-01 20:03:10 +0300

    button

--- swe466_project/lib/screens/sharedUI.dart
+++ swe466_project/lib/screens/sharedUI.dart
@@ -10,3 +10,27 @@ Widget getTextFiled(controller, labelText, width) {
     ),
   );
 }
+
+class CustomButton extends StatelessWidget {
+  CustomButton(this.onPressed, this.text);
+  final GestureTapCallback onPressed;
+  String text;
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Colors.blue,
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: onPressed,
+        child: Text(text,
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Color(0xff152a55), fontWeight: FontWeight.bold)),
+      ),
+    );
+  }
+}
