e51544227a756d81dbfe723e7a5e8db423fc9ed5
Lama <lamayf@hotmail.com>
2021-04-02 04:25:50 +0300

    Revert "Revert "update textfield style""
    This reverts commit c2ebca339d6f2c024c99695a6e9f5416b9ea7bb5.

--- swe466_project/lib/router.dart
+++ swe466_project/lib/router.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:swe466_project/screens/addTasks.dart';
 import 'package:swe466_project/screens/home.dart';
 
-Route<dynamic> generateRoute(RouteSettings settings){
-  switch(settings.name){
+Route<dynamic> generateRoute(RouteSettings settings) {
+  switch (settings.name) {
     case "/":
-      return MaterialPageRoute(builder: (context) => Home());
+      return MaterialPageRoute(builder: (context) => AddTasks());
     default:
       return MaterialPageRoute(builder: (context) => Home());
-  }//end switch
+  } //end switch
 }
